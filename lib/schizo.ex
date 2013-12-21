fb8753c19c01c603f779ac9155b7f6d59b159e2f
Darko Fabijan <darko@renderedtext.com>
2013-12-21 15:56:23 +0100

    DRYed some parts.

--- lib/schizo.ex
+++ lib/schizo.ex
@@ -1,33 +1,31 @@
 defmodule Schizo do
 
   def uppercase(string) do
-    words = String.split(string)
-
-    words_with_index = Stream.with_index(words)
-
-    transformed_words = Stream.map(words_with_index, &transform/1)
-
-    Enum.join(transformed_words, " ")
+    transform_every_other_word(string, &uppercase_transformation/1)
   end
 
   def unvowel(string) do
+    transform_every_other_word(string, &vowels_transformation/1)
+  end
+
+  defp transform_every_other_word(string, transformation_function) do
     words = String.split(string)
 
     words_with_index = Stream.with_index(words)
 
-    transformed_words = Stream.map(words_with_index, &remove_vowels/1)
+    transformed_words = Stream.map(words_with_index, transformation_function)
 
     Enum.join(transformed_words, " ")
   end
 
-  defp transform({word, index}) do
+  defp uppercase_transformation({word, index}) do
     cond do
       rem(index, 2) == 0 -> word
       rem(index, 2) == 1 -> String.upcase(word)
     end
   end
 
-  defp remove_vowels({word, index}) do
+  defp vowels_transformation({word, index}) do
     cond do
       rem(index, 2) == 0 -> word
       rem(index, 2) == 1 -> Regex.replace(%r/[aeiou]/, word, "")
