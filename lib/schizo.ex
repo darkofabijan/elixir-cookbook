64b732dc024e6d2b0f1b0f5a5ed610bae3601067
Darko Fabijan <darko@renderedtext.com>
2013-12-21 15:30:07 +0100

    Implementaion through duplication.

--- lib/schizo.ex
+++ lib/schizo.ex
@@ -1,13 +1,21 @@
 defmodule Schizo do
 
   def uppercase(string) do
-    string
-
     words = String.split(string)
 
     words_with_index = Stream.with_index(words)
 
     transformed_words = Stream.map(words_with_index, &transform/1)
+
+    Enum.join(transformed_words, " ")
+  end
+
+  def unvowel(string) do
+    words = String.split(string)
+
+    words_with_index = Stream.with_index(words)
+
+    transformed_words = Stream.map(words_with_index, &remove_vowels/1)
 
     Enum.join(transformed_words, " ")
   end
@@ -19,6 +27,11 @@ defmodule Schizo do
     end
   end
 
-
+  defp remove_vowels({word, index}) do
+    cond do
+      rem(index, 2) == 0 -> word
+      rem(index, 2) == 1 -> Regex.replace(%r/[aeiou]/, word, "")
+    end
+  end
 
 end
--- test/schizo_test.exs
+++ test/schizo_test.exs
@@ -9,4 +9,16 @@ defmodule SchizoTest do
     assert(Schizo.uppercase("foo bar") == "foo BAR")
   end
 
+  test "unvowel doesn't change the first word" do
+    assert(Schizo.uppercase("foo") == "foo")
+  end
+
+  test "unvowel removes the second word's vowels" do
+    assert(Schizo.unvowel("foo bar") == "foo br")
+  end
+
+  test "unvowel removes every other word's vowels" do
+    assert(Schizo.unvowel("foo bar joo maa") == "foo br joo m")
+  end
+
 end
